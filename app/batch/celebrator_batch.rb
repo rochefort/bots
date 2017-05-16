d1dde16e200de3e2d361f3793706745f288b4647
rochefort <terasawan@gmail.com>
2017-05-16 16:30:14 +0900

    Implement celebration
    (#e5dc197) の実装が途中までになっていたのを実装しています。

--- app/batch/celebrator_batch.rb
+++ app/batch/celebrator_batch.rb
@@ -1,37 +1,45 @@
 class CelebratorBatch
-  def self.celebrate_birth_of_month
-    Account.all.each do |account|
-      account = Account.first
-      celebs = account.players.select { |pl| pl.birth_date.month == Date.today.month }.sort_by { |pl| pl.birth_date.strftime("%m%d") }
-      messages = celebs.map do |player|
-        number = "%2d" % player.number
-        "#{player.position} #{number} #{player.name} 選手(#{player.birth_date})"
-      end
-
-      unless messages.empty?
-        puts "今月お誕生日を迎える選手です！"
-        puts
-        puts messages.join("\n")
-        puts
-        puts "おめでとうございます。"
+  class << self
+    def celebrate_birth_of_month
+      Account.all.each do |account|
+        celebs = account.players.select { |pl| pl.birth_date.month == Date.today.month }.sort_by { |pl| pl.birth_date.strftime("%m%d") }
+        body = celebs.map do |player|
+          number = "%2d" % player.number
+          "#{player.position} #{number} #{player.name} 選手(#{player.birth_date})"
+        end
+        msg = generate_birth_of_month_message(body)
+        client = TwitterClient.new(account)
+        client.tweet(msg)
       end
     end
-  end
 
-  def self.celebrate_birth
-    Account.all.each do |account|
-      account = Account.first
-      celebs = account.players.select { |pl| pl.birth_date.month == Date.today.month && pl.birth_date.day == Date.today.day }
-      messages = celebs.map do |player|
-        number = "%2d" % player.number
-        "#{player.position} #{number} #{player.name} 選手(#{player.birth_date})"
-      end
-
-      unless messages.empty?
-        puts "お誕生日おめでてとうございます！"
-        puts
-        puts messages.join("\n")
+    def celebrate_birth
+      Account.all.each do |account|
+        celebs = account.players.select { |pl| pl.birth_date.month == Date.today.month && pl.birth_date.day == Date.today.day }
+        messages = celebs.map do |player|
+          number = "%2d" % player.number
+          "#{player.position} #{number} #{player.name} 選手(#{player.birth_date})"
+        end
+        msg = generate_birth_of_month_message(body)
+        client = TwitterClient.new(account)
+        client.tweet(msg)
       end
     end
-  end
+
+    private
+      def generate_birth_of_month_message(body)
+        return "" if body.empty?
+        "今月お誕生日を迎える選手です！\n" +
+        "おめでとうございます。\n" +
+        "\n" +
+        body.join("\n") + "\n"
+      end
+
+      def generate_birth_message(body)
+        return "" if body.empty?
+        "お誕生日おめでてとうございます！\n" +
+        "\n" +
+        body.join("\n")
+      end
+    end
 end
